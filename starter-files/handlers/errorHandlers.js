50c7142078c9c13d7b6975475f77825f43a2bf1b
Marcus Crane <marcus@thingsima.de>
2017-05-11 18:15:22 +1200

    Typo fix
    Changed "catch and errors" to "catch any errors"

--- starter-files/handlers/errorHandlers.js
+++ starter-files/handlers/errorHandlers.js
@@ -3,7 +3,7 @@
 
   With async/await, you need some way to catch errors
   Instead of using try{} catch(e) {} in each controller, we wrap the function in
-  catchErrors(), catch and errors they throw, and pass it along to our express middleware with next()
+  catchErrors(), catch any errors they throw, and pass it along to our express middleware with next()
 */
 
 exports.catchErrors = (fn) => {

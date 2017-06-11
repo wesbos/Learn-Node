15f2739df7d9b91cc3860288020eb13cc498f57d
andrew.muntyan <andrew.muntyan@nextiva.com>
2017-06-11 19:10:02 +0300

    one more registration with same email fails silently

--- stepped-solutions/25/routes/index.js
+++ stepped-solutions/25/routes/index.js
@@ -35,7 +35,11 @@ router.get('/register', userController.registerForm);
 // 3. we need to log them in
 router.post('/register',
   userController.validateRegister,
-  userController.register,
+  // we need to know about errors if 
+  // validation will be passed, but registration 
+  // will be failed in some reasons, e.g. second 
+  // registration with same email
+  catchErrors(userController.register),
   authController.login
 );
 

88902e6ee42d9dcc9f75f58d11318a596caddde7
Riley Bracken <rileybracken@gmail.com>
2017-06-12 23:07:03 -0600

    This updates the `tagQuery` to handle empty arrays.
    This is because arrays in MongoDB (I believe) will be created with
    empty values i.e. `[]`. In MongoDB's docs it says "$exists matches the
    documents that contain the field, including documents where the field
    value is null". `[] === null` is false.

--- stepped-solutions/29/controllers/storeController.js
+++ stepped-solutions/29/controllers/storeController.js
@@ -84,7 +84,7 @@ exports.getStoreBySlug = async (req, res, next) => {
 
 exports.getStoresByTag = async (req, res) => {
   const tag = req.params.tag;
-  const tagQuery = tag || { $exists: true };
+  const tagQuery = tag || { tags: { $exists: true, $ne: [] } };
 
   const tagsPromise = Store.getTagsList();
   const storesPromise = Store.find({ tags: tagQuery });

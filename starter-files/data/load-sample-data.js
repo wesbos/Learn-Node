3c450eb0a009b7f115cb8dba306ee1fc6856c66e
Tom Hutchinson <tom.hutchinson@beamly.com>
2017-09-01 16:52:44 +0100

    allow load-sample-data to run before Review model exists

--- starter-files/data/load-sample-data.js
+++ starter-files/data/load-sample-data.js
@@ -7,19 +7,31 @@ mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
 
 // import all of our models - they need to be imported only once
 const Store = require('../models/Store');
-const Review = require('../models/Review');
 const User = require('../models/User');
 
+let hasReviewModel;
+let Review;
+try {
+  Review = require('../models/Review');
+  hasReviewModel = true;
+} catch(e) {
+  if (e.message === "Cannot find module '../models/Review'") {
+    hasReviewModel = false;
+  } else {
+    console.log(e);
+    process.exit();
+  }
+}
 
 const stores = JSON.parse(fs.readFileSync(__dirname + '/stores.json', 'utf-8'));
-const reviews = JSON.parse(fs.readFileSync(__dirname + '/reviews.json', 'utf-8'));
 const users = JSON.parse(fs.readFileSync(__dirname + '/users.json', 'utf-8'));
+const reviews = JSON.parse(fs.readFileSync(__dirname + '/reviews.json', 'utf-8'));
 
 async function deleteData() {
   console.log('😢😢 Goodbye Data...');
   await Store.remove();
-  await Review.remove();
   await User.remove();
+  if (hasReviewModel) await Review.remove();
   console.log('Data Deleted. To load sample data, run\n\n\t npm run sample\n\n');
   process.exit();
 }
@@ -27,8 +39,8 @@ async function deleteData() {
 async function loadData() {
   try {
     await Store.insertMany(stores);
-    await Review.insertMany(reviews);
     await User.insertMany(users);
+    if (hasReviewModel) await Review.insertMany(reviews);
     console.log('👍👍👍👍👍👍👍👍 Done!');
     process.exit();
   } catch(e) {

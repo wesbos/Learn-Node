34aa6086bf77ec5741e5f1ec515f801e89977552
Madison Kanna <madisonkanna@gmail.com>
2017-10-03 14:48:21 -0700

    Fix typo
    handle 'an' bad connections' > handle 'any' bad connections

--- starter-files/start.js
+++ starter-files/start.js
@@ -10,7 +10,7 @@ if (major < 7 || (major === 7 && minor <= 5)) {
 // import environmental variables from our variables.env file
 require('dotenv').config({ path: 'variables.env' });
 
-// Connect to our Database and handle an bad connections
+// Connect to our Database and handle any bad connections
 mongoose.connect(process.env.DATABASE);
 mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
 mongoose.connection.on('error', (err) => {

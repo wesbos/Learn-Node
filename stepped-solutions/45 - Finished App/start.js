eecf1dc2d394a1c04b8911e0c816e2aeffc8138e
Briggs Elsperger <briggs.elsperger@hotmail.com>
2018-01-30 14:07:40 -0600

    Fixes typo

--- stepped-solutions/45 - Finished App/start.js
+++ stepped-solutions/45 - Finished App/start.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // import environmental variables from our variables.env file
 require('dotenv').config({ path: 'variables.env' });
 
-// Connect to our Database and handle an bad connections
+// Connect to our Database and handle any bad connections
 mongoose.connect(process.env.DATABASE);
 mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
 mongoose.connection.on('error', (err) => {

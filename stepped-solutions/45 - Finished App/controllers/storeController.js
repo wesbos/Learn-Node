3b222cbdc43ee4c076fd40046c5b37587bfec01d
Ashiqur Rahman <ashiquejava@gmail.com>
2018-11-26 01:17:59 +0600

    fixes the store page link in flash message.

--- stepped-solutions/45 - Finished App/controllers/storeController.js
+++ stepped-solutions/45 - Finished App/controllers/storeController.js
@@ -99,7 +99,7 @@ exports.updateStore = async (req, res) => {
     new: true, // return the new store instead of the old one
     runValidators: true
   }).exec();
-  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store →</a>`);
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/store/${store.slug}">View Store →</a>`);
   res.redirect(`/stores/${store._id}/edit`);
   // Redriect them the store and tell them it worked
 };

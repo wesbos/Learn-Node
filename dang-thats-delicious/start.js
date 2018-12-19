402ea897d806e8768e48916a2278b9fcce085c44
Robert Schulte <schulte.robertw@outlook.com>
2018-12-18 19:28:49 -0600

    commit

--- dang-thats-delicious/start.js
+++ dang-thats-delicious/start.js
@@ -1,12 +1,5 @@
 const mongoose = require('mongoose');
 
-// Make sure we are running node 7.6+
-const [major, minor] = process.versions.node.split('.').map(parseFloat);
-if (major < 7 || (major === 7 && minor <= 5)) {
-  console.log('🛑 🌮 🐶 💪 💩\nHey You! \n\t ya you! \n\t\tBuster! \n\tYou\'re on an older version of node that doesn\'t support the latest and greatest things we are learning (Async + Await)! Please go to nodejs.org and download version 7.6 or greater. 👌\n ');
-  process.exit();
-}
-
 // import environmental variables from our variables.env file
 require('dotenv').config({ path: 'variables.env' });
 

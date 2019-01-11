7194a232778b69907aafd11b116c4d0f4eb28acb
Robert Schulte <schulte.robertw@outlook.com>
2019-01-11 17:24:56 -0600

    registration and auto-login works

--- dang-thats-delicious/controllers/authController.js
+++ dang-thats-delicious/controllers/authController.js
@@ -1,5 +1,8 @@
-const mongoose = require('mongoose')
+const passport = require('passport')
 
-exports.login = (req, res) => {
-    console.log(req.body)
-}
+exports.login = passport.authenticate('local', {
+    failureRedirect: '/login',
+    failureFlash: 'Failed Login!',
+    successRedirect: '/',
+    successFlash: 'You are now logged in!'
+})

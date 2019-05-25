14dd06a73f8e3dfb09dde9ffb60665029724467d
Ryan M <ryanmarion012@gmail.com>
2019-05-25 13:20:06 -0600

    put together a couple of basic routes, installed clean, things are working.

--- node_proj/routes/index.js
+++ node_proj/routes/index.js
@@ -3,7 +3,13 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  const ryan = {name:"Ryan",color:"white",blah:"blah"};
+  res.json(ryan);
+});
+
+router.get('/reverse/:name', (req,res)=> {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
 });
 
 module.exports = router;

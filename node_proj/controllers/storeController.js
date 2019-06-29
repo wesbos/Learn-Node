562afb4ba54ca94218b323e17ed0062c28f8936e
Ryan M <ryanmarion012@gmail.com>
2019-06-28 21:48:14 -0600

    editing flow completed

--- node_proj/controllers/storeController.js
+++ node_proj/controllers/storeController.js
@@ -21,3 +21,24 @@ exports.getStores =  async (req,res) => {
   // console.log(stores);
   res.render('stores',{title:'Stores',stores:stores});
 };
+
+exports.editStore = async (req,res) => {
+  //find the store given the ID
+  const store = await Store.findOne({ _id:req.params.id });
+  //confirm they are the owner
+  //TODO
+  //render out hte edit form
+  res.render('editStore',{title:`Edit ${store.name}`,store});
+};
+
+exports.updateStore = async (req,res) => {
+  //find and update the store
+  const store = await Store.findOneAndUpdate({_id:req.params.id},req.body,{
+    new:true, //return new store instead of the old one
+    runValidators:true, //force to adhere to the store model
+  }).exec();
+  req.flash('success',`Successfully updated <strong>${store.name}</strong>
+  <a href="/stores/${store.slug}">View Store ->`);
+  res.redirect(`/stores/${store._id}/edit`);
+  //redirect them to the store, tell them it worked
+}
--- node_proj/routes/index.js
+++ node_proj/routes/index.js
@@ -8,5 +8,7 @@ router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
 router.get('/add', storeController.addStore);
 router.post('/add',catchErrors(storeController.createStore));
+router.post('/add/:id',catchErrors(storeController.updateStore));
+router.get('/stores/:id/edit',catchErrors(storeController.editStore));
 
 module.exports = router;

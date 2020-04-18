7ce99752ad0f8528afaf637deed41d482c267517
JonathonLLopez <jonathonllopez@gmail.com>
2020-04-17 23:03:06 -0500

    added data model for Store

--- gittin-bread/routes/index.js
+++ gittin-bread/routes/index.js
@@ -2,12 +2,6 @@ const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
 
-// Do work here
 router.get('/', storeController.homePage);
-
-router.get('/reverse/:name', (req, res) => {
-  const reverse = [...req.params.name].reverse().join('')
-  res.send(reverse);
-})
 
 module.exports = router;
--- gittin-bread/start.js
+++ gittin-bread/start.js
@@ -14,6 +14,8 @@ mongoose.connection.on('error', (err) => {
 
 // READY?! Let's go!
 
+//import all models
+require('./models/Store');
 
 // Start our app!
 const app = require('./app');

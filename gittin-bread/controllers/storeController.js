dde3bacc23c17828d0db7878c19ce6964f6bc347
JonathonLLopez <jonathonllopez@gmail.com>
2020-04-17 23:35:11 -0500

    added new pug file to views and added controller method

--- gittin-bread/controllers/storeController.js
+++ gittin-bread/controllers/storeController.js
@@ -1,3 +1,11 @@
 exports.homePage = (req, res) => {
   res.render('index');
 }
+
+exports.addStore = (req, res) => {
+  res.render('editStore', {title: 'Add Store'});
+}
+
+exports.createStore = (req, res) => { 
+  res.json(req.body);
+}
--- gittin-bread/routes/index.js
+++ gittin-bread/routes/index.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const storeController = require('../controllers/storeController');
 
 router.get('/', storeController.homePage);
+router.get('/add', storeController.addStore);
+
+router.post('/add', storeController.createStore);
 
 module.exports = router;

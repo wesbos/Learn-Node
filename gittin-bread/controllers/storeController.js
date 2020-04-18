d87adab3de08a79972668d50215f4141e310fe94
JonathonLLopez <jonathonllopez@gmail.com>
2020-04-18 00:01:21 -0500

    added new controller to save data to db

--- gittin-bread/controllers/storeController.js
+++ gittin-bread/controllers/storeController.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
 exports.homePage = (req, res) => {
   res.render('index');
 }
@@ -6,6 +8,9 @@ exports.addStore = (req, res) => {
   res.render('editStore', {title: 'Add Store'});
 }
 
-exports.createStore = (req, res) => { 
-  res.json(req.body);
+exports.createStore = async (req, res) => { 
+  const store = new Store(req.body);
+  await store.save()
+  console.log('worked')
+  res.redirect('/');
 }

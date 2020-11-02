a25c8bad195b5a7c76a31eb19b99d3e1ec65f9d4
JonathonLLopez <jonathonllopez@gmail.com>
2020-11-01 19:10:40 -0600

    set up email sending and email auth with mailtrap

--- gittin-bread/controllers/authController.js
+++ gittin-bread/controllers/authController.js
@@ -3,6 +3,7 @@ const crypto = require('crypto')
 const mongoose = require('mongoose')
 const User = mongoose.model('User')
 const promisify = require('es6-promisify');
+const mail = require('../handlers/mail');
 
 exports.login = passport.authenticate('local', {
   failureRedirect: '/login',
@@ -39,8 +40,14 @@ exports.forgot = async (req, res) => {
   user.resetPasswordExpires = Date.now() + 3600000
   await user.save()
   //redirect to login page
-  const resetUrl = `http://${req.headers.host}/account/reset/${user.resetPasswordToken}`
-  req.flash('success', `You have been emailed a reset link at ${resetUrl}`)
+  const resetURL = `http://${req.headers.host}/account/reset/${user.resetPasswordToken}`
+  await mail.send({
+    user,
+    filename: 'password-reset',
+    subject: 'Password Reset',
+    resetURL
+  });
+  req.flash('success', `You have been emailed a password reset link.`);
   res.redirect('/login')
 }
 

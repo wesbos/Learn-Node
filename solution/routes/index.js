47582b1095051f47596faf82c8dcd9e8bc2877ec
Petar Dzhambazov <petar.dzhambazov@ezbob.com>
2020-12-04 20:35:45 +0200

    first step

--- solution/routes/index.js
+++ solution/routes/index.js
@@ -1,9 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+router.get("/", (req, res) => {
+  console.log("Hey from /");
+  res.render('hello', {dog: req.query.dog})
 });
 
+router.get("/reverse/:name", (req, res) => {
+  res.send(req.params.name.split('').reverse().join(''));
+});
 module.exports = router;

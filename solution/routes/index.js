e450a2514d6e48bbec54c98f8c0b38f86ac97963
Petar Dzhambazov <jamby77@gmail.com>
2020-12-04 23:20:52 +0200

    catch up with some changes

--- solution/routes/index.js
+++ solution/routes/index.js
@@ -1,13 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const storeController = require("../controllers/storeController");
 
 // Do work here
-router.get("/", (req, res) => {
-  console.log("Hey from /");
-  res.render('hello', {dog: req.query.dog})
-});
+router.get("/", storeController.homePage);
 
-router.get("/reverse/:name", (req, res) => {
-  res.send(req.params.name.split('').reverse().join(''));
-});
 module.exports = router;

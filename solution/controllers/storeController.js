a4587ed370b3e0cfb96217f530c1f85e4021d786
Petar Dzhambazov <jamby77@gmail.com>
2020-12-05 23:15:07 +0200

    add flash message on save

--- solution/controllers/storeController.js
+++ solution/controllers/storeController.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const Store = mongoose.model("Store");
 exports.homePage = (req, res) => {
-  console.log(req.name);
   res.render("index");
 };
 
@@ -12,5 +11,9 @@ exports.addStore = (req, res) => {
 exports.createStore = async (req, res) => {
   const store = new Store(req.body);
   const result = await store.save();
-  res.redirect("/");
+  req.flash(
+    "success",
+    `Successfully created ${result.name}. Care to     leave a review`
+  );
+  res.redirect(`/store/${result.slug}`);
 };

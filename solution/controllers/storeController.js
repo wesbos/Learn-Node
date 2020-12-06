9a86a2572355c0ff2c0338733499d15298b16a9c
Petar Dzhambazov <jamby77@gmail.com>
2020-12-06 21:44:59 +0200

    store editing

--- solution/controllers/storeController.js
+++ solution/controllers/storeController.js
@@ -4,7 +4,6 @@ const Store = mongoose.model("Store");
 exports.getStores = async (req, res) => {
   // query db for all stores
   const stores = await Store.find();
-  console.log(stores);
   res.render("stores", { title: "Stores", stores });
 };
 
@@ -21,7 +20,28 @@ exports.createStore = async (req, res) => {
   const result = await store.save();
   req.flash(
     "success",
-    `Successfully created ${result.name}. Care to     leave a review`
+    `Successfully created ${result.name}. Care to leave a review`
   );
   res.redirect(`/store/${result.slug}`);
+};
+
+exports.updateStore = async (req, res) => {
+  const { storeId } = req.params;
+  console.log("about to update", storeId);
+  const result = await Store.findOneAndUpdate({ _id: storeId }, req.body, {
+    new: true, // return updated store
+    runValidators: true,
+  }).exec();
+  req.flash(
+    "success",
+    `Successfully updated <strong>${result.name}</strong>. <a href="/store/${result.slug}">View store -></a>`
+  );
+  res.redirect(`/stores/${result.id}/edit`);
+};
+
+exports.editStore = async (req, res) => {
+  const { storeId } = req.params;
+  const store = await Store.findById(storeId);
+  // todo confirm rights to edit store
+  res.render("editStore", { title: `Edit ${store.name}`, store });
 };
--- solution/routes/index.js
+++ solution/routes/index.js
@@ -8,5 +8,7 @@ router.get("/", catchErrors(storeController.getStores));
 router.get("/stores", catchErrors(storeController.getStores));
 router.get("/add", storeController.addStore);
 router.post("/add", catchErrors(storeController.createStore));
+router.get("/stores/:storeId/edit", catchErrors(storeController.editStore));
+router.post("/update/:storeId", catchErrors(storeController.updateStore));
 
 module.exports = router;

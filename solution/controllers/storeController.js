2dad829ce92ecbbf3b2f68d73dedd16f73abde3f
Petar Dzhambazov <petar.dzhambazov@ezbob.com>
2020-12-11 18:13:25 +0200

    show tag stores

--- solution/controllers/storeController.js
+++ solution/controllers/storeController.js
@@ -49,8 +49,8 @@ exports.createStore = async (req, res) => {
   const store = new Store(req.body);
   const result = await store.save();
   req.flash(
-      'success',
-      `Successfully created ${ result.name }. Care to leave a review`
+    'success',
+    `Successfully created ${ result.name }. Care to leave a review`
   );
   res.redirect(`/stores/${ result.slug }`);
 };
@@ -62,24 +62,24 @@ exports.updateStore = async (req, res) => {
     store[key] = req.body[key];
   });
   console.log('about to update', storeId);
-  store.location.type = 'Point'
+  store.location.type = 'Point';
   const result = await store.save();
   // const result = await Store.findOneAndUpdate({ _id: storeId }, req.body, {
   //   new: true, // return updated store
   //   runValidators: true,
   // }).exec();
   req.flash(
-      'success',
-      `Successfully updated <strong>${ result.name }</strong>. <a href="/stores/${ result.slug }">View store -></a>`
+    'success',
+    `Successfully updated <strong>${ result.name }</strong>. <a href="/stores/${ result.slug }">View store -></a>`
   );
   res.redirect(`/stores/${ result.id }/edit`);
 };
 
 exports.editStore = async (req, res) => {
-  const { storeId } = req.params;
+  const {storeId} = req.params;
   const store = await Store.findById(storeId);
   // todo confirm rights to edit store
-  res.render("editStore", { title: `Edit ${store.name}`, store });
+  res.render('editStore', {title: `Edit ${ store.name }`, store});
 };
 
 exports.showStore = async (req, res, next) => {
@@ -91,7 +91,13 @@ exports.showStore = async (req, res, next) => {
 };
 
 exports.getStoresByTag = async (req, res) => {
-  const tags = await Store.getTagsList();
-  const selectedTag = req.params.tag;
-  res.render('tags', {tags, title: 'Tags', tag: selectedTag || null});
+  const tag = req.params.tag;
+  const tagsPromise = Store.getTagsList();
+  let filter = {tags: {$exists: true}};
+  if (tag) {
+    filter.tags = tag;
+  }
+  const storesPromise = Store.find(filter);
+  const [tags, stores] = await Promise.all([tagsPromise, storesPromise]);
+  res.render('tags', {tags, title: 'Tags', tag: tag || null, stores});
 };
